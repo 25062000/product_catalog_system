a8c27766733aca5489529c2be543eaad5469f4cc
Siva Priya <sivapriyaram777@gmail.com>
2022-12-12 01:16:30 -0800

    login and register commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class='row container-well'>
+        <div class='col-6 d-flex align-items-center justify-content-center'>
+        <a class="btn btn-primary" href="#" role="button">View Products</a>
+       
+        </div>
+        <div class='col-6'>
+        <img src="http://surl.li/eafuc" class="img-fluid " alt="image">
+        </div>  
+<div>
+@endsection
